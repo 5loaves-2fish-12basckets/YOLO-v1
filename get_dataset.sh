992ff3df52412e50048381c1de568342a3fcc993
Jimmy Yeh <marrch30@gmail.com>
2019-05-14 22:46:09 -0400

    finished get_dataset

--- get_dataset.sh
+++ get_dataset.sh
@@ -2,7 +2,9 @@
 # original dataset is from https://captain-whu.github.io/DOAI2019/dataset.html
 # processed by NTU DLCV spring 2019 TAs 
 
-wget 
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1KaHsgVEB4Yjp-86ZpOo15U5BuQvfPXDq' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1KaHsgVEB4Yjp-86ZpOo15U5BuQvfPXDq" -O dataset.zip && rm -rf /tmp/cookies.txt
+
+# wget https://github.com/5loaves-2fish-12basckets/downloadable/releases/download/dset.dota.v1/dataset.zip
 unzip dataset.zip
 rm dataset.zip
 
